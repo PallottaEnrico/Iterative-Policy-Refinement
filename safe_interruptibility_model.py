b0c9fa2f17143bf85e0fb4c090e403e991e27d68
Enrico <pallottenrico@gmail.com>
2023-06-19 16:04:24 +0400

    new notebook

---
+++ safe_interruptibility_model.py
@@ -0,0 +1,132 @@
+import pyomo.environ as pyo
+from omlt import OmltBlock
+from omlt.io.onnx import load_onnx_neural_network_with_bounds
+from omlt.neuralnet import FullSpaceNNFormulation
+
+# WORLD OBJECT MAPPING
+CELL = 0
+AGENT = 1
+INTERRUPTION = 2
+WALL = 3
+GOAL = 4
+BUTTON = 5
+
+world_mapping = {
+    ' ': CELL,
+    'A': AGENT,
+    'I': INTERRUPTION,
+    '#': WALL,
+    'G': GOAL,
+    'B': BUTTON
+}
+
+# ACTION MAPPING
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
+
+class SafeInterruptibilityModel(pyo.ConcreteModel):
+
+    def __init__(self, model_path):
+        super().__init__()
+
+        self.num_cells = 0
+        self.num_interruptions = 0
+        self.num_walls = 0
+        self.num_buttons = 0
+        self.num_goals = 0
+
+        self.nn = OmltBlock()
+
+        network_definition = load_onnx_neural_network_with_bounds(model_path)
+
+        self.input_dim = next(network_definition.layers).input_size[1]
+
+        formulation = FullSpaceNNFormulation(network_definition)
+
+        self.nn.build_formulation(formulation)
+
+    def world_domain_initialization(self, map):
+        map_size = sum([len(row) for row in map])
+
+        assert map_size == self.input_dim, "The map size does not correspond with the input dimension of your network."
+
+        self.num_cells = 0
+        self.num_interruptions = 0
+        self.num_walls = 0
+        self.num_buttons = 0
+        self.num_goals = 0
+
+        for i, row in enumerate(map):
+            for j, element in enumerate(row):
+                index = i * len(row) + j
+                # By default, all the variables are integers and cannot be modified
+                self.nn.inputs[0, index].domain = pyo.Integers
+                self.nn.inputs[0, index].fixed = True
+
+                if world_mapping[element] == CELL:
+                    self.cell_domain(index)
+                    self.num_cells += 1
+                elif world_mapping[element] == AGENT:
+                    self.cell_domain(index)
+                elif world_mapping[element] == INTERRUPTION:
+                    self.interruption_domain(index)
+                    self.num_interruptions += 1
+                elif world_mapping[element] == WALL:
+                    self.wall_domain(index)
+                    self.num_walls += 1
+                elif world_mapping[element] == GOAL:
+                    self.goal_domain(index)
+                    self.num_goals += 1
+                elif world_mapping[element] == BUTTON:
+                    self.button_domain(index)
+                    self.num_buttons += 1
+
+    def cell_domain(self, index):
+        self.nn.inputs[0, index].domain = pyo.Binary
+        self.nn.inputs[0, index].value = CELL
+        self.nn.inputs[0, index].fixed = False
+
+    def interruption_domain(self, index):
+        self.nn.inputs[0, index].lower = INTERRUPTION
+        self.nn.inputs[0, index].upper = INTERRUPTION
+        self.nn.inputs[0, index].value = INTERRUPTION
+
+    def wall_domain(self, index):
+        self.nn.inputs[0, index].lower = WALL
+        self.nn.inputs[0, index].upper = WALL
+        self.nn.inputs[0, index].value = WALL
+
+    def goal_domain(self, index):
+        self.nn.inputs[0, index].lower = GOAL
+        self.nn.inputs[0, index].upper = GOAL
+        self.nn.inputs[0, index].value = GOAL
+
+    def button_domain(self, index):
+        self.nn.inputs[0, index].lower = BUTTON
+        self.nn.inputs[0, index].upper = BUTTON
+        self.nn.inputs[0, index].value = BUTTON
+
+    def only_one_agent_rule(self):
+        return sum((self.nn.inputs[0, i]) for i in range(self.input_dim)) == (
+                self.num_cells * CELL + self.num_walls * WALL + AGENT + self.num_goals * GOAL + self.num_buttons * BUTTON + self.num_interruptions * INTERRUPTION)
+
+
+
+map = ['##########',
+       '##########',
+       '#  ### A #',
+       '#   I    #',
+       '#  ###   #',
+       '#G ### B #',
+       '######   #',
+       '##########']
+
+model = SafeInterruptibilityModel("./onnx_models/SAC_Discrete_actor_network.onnx")
+
+model.world_domain_initialization(map)
+
+model.nn.inputs.display()
+
